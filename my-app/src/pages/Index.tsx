4808df79f7776565bd451c17b3226dbe88477e8f
wanglu <781550203@qq.com>
2019-07-12 15:23:20 +0800

    倒计时

--- my-app/src/pages/Index.tsx
+++ my-app/src/pages/Index.tsx
@@ -5,6 +5,7 @@ import './Index.less'
 
 export interface IndexProps { 
   newDate: any
+  countDownTime: any
 }
 const mapStateToProps = (state: any) => ({
   count: state.count
@@ -18,16 +19,20 @@ type Props = Partial<connectedProps>
 
 class Index extends React.Component<Props, IndexProps> {
   timer?: any;
+  countDown?: any;
   state: Readonly<IndexProps> = {
-    newDate: new Date()
+    newDate: new Date(),
+    countDownTime: ''
   }
   componentDidMount() {
     this.timer = setInterval(() => this.tick(),1000)
     const { countDispatch } = this.props;
     countDispatch && countDispatch({ num: 1 })
+    this.getCountDown(10)
   }
   componentWillUnmount(){
     clearInterval(this.timer)
+    clearInterval(this.countDown)
   }
   tick(){
     this.setState({ newDate: new Date() });
@@ -38,6 +43,7 @@ class Index extends React.Component<Props, IndexProps> {
     return (
       <div className="IndexContent">
         <p className="time" onClick={this.goMoment}>当前时间: {newDate.toLocaleTimeString()}</p>
+        <p id="countDown1">获取倒计时: {this.state.countDownTime}</p>
         <p>{count.num}</p>
         <button onClick={this.goMy}>+</button>
         <button onClick={this.goLodash}>Lodash</button>
@@ -69,6 +75,30 @@ class Index extends React.Component<Props, IndexProps> {
   }
   goProduct = () => {
     history.push('./product')
+  }
+  getCountDown = (timestamp: any) => {
+    let countDownTime
+    setInterval(() => {
+      if(timestamp === 0){
+        return '0 天 ：00：00：00'
+      }
+      timestamp --
+      const days = parseInt(timestamp / (60 * 60 * 24) + '')
+      let hours: any = parseInt((timestamp % (60 * 60 * 24)) / (60 * 60) + '')
+      let minutes: any = parseInt((timestamp % (60 * 60)) / 60 + '')
+      let seconds: any = parseInt(timestamp % 60 + '')
+      if (hours < 10) {
+        hours = '0' + hours;
+      }
+      if (minutes < 10) {
+        minutes =  '0' + minutes;
+      }
+      if (seconds < 10) {
+        seconds = '0' + seconds;
+      }
+     countDownTime = days + '天' + hours + ":" + minutes + ":" + seconds;
+     this.setState({countDownTime})
+   },1000);
   }
 }
 export default connect(
