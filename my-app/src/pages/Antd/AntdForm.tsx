d9a5bdd88518fe4fe15b4556bb98de1773420fc9
wanglu <781550203@qq.com>
2019-11-06 11:08:06 +0800

    feat: swiper demo

--- my-app/src/pages/Antd/AntdForm.tsx
+++ my-app/src/pages/Antd/AntdForm.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
-import { Button, Form, Input, InputNumber, Select } from 'antd'
+import { Button, Form, Input, InputNumber, Select, DatePicker } from 'antd';
 const FormItem = Form.Item;
 import { FormComponentProps } from 'antd/lib/form/Form';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+moment.locale('zh-cn');
+
+const { RangePicker } = DatePicker;
+
+function range(start, end) {
+  const result: Array<number> = [];
+  for (let i = start; i < end; i++) {
+    result.push(i);
+  }
+  return result;
+}
+
+function disabledDate(current) {
+  // Can not select days before today and today
+  return current && current < moment().endOf('day');
+}
+
+function disabledRangeTime(_, type) {
+  if (type === 'start') {
+    return {
+      disabledHours: () => range(0, 60).splice(4, 20),
+      disabledMinutes: () => range(30, 60),
+      disabledSeconds: () => [55, 56],
+    };
+  }
+  return {
+    disabledHours: () => range(0, 60).splice(20, 4),
+    disabledMinutes: () => range(0, 31),
+    disabledSeconds: () => [55, 56],
+  };
+}
 
 function onFieldsChange(props: any, fields: any) {
   props.onChange(fields);
 }
-function mapPropsToFields(props: any){
+function mapPropsToFields(props: any) {
   console.log('props', props.value);
   return {
-    value: props.value
-  }
+    value: props.value,
+  };
 }
+
 interface formType {
   name: string;
   age: number;
@@ -21,17 +55,17 @@ interface Props extends FormComponentProps {
   saveInfo: (params: formType) => void;
   onChange: (data: formType) => void;
 }
-class AntdForm extends React.Component<Props>{
-  node: any
-  checkAge = (rule: any, value: any, callback:(value?: any)=>void) => {
+class AntdForm extends React.Component<Props> {
+  node: any;
+  checkAge = (rule: any, value: any, callback: (value?: any) => void) => {
     const number = parseInt(value || 0, 10);
-    if(Number.isNaN(value)){
+    if (Number.isNaN(value)) {
       callback('请输入数字');
-      return; 
+      return;
     }
-    callback()
-  }
-  componentDidMount(){
+    callback();
+  };
+  componentDidMount() {
     // console.log('this.ref', this.node.getBoundingClientRect())
     // window.addEventListener('resize', (e) => {
     //   if (window.innerWidth < 1280) {
@@ -50,28 +84,28 @@ class AntdForm extends React.Component<Props>{
     //   }
     // })
   }
-  render(){
+  render() {
     const { getFieldDecorator } = this.props.form;
-    const nameItem = getFieldDecorator('name', {rules: [{len: 24, required: true}]})(<Input style={{width: '300px'}} placeholder="请输入姓名"/>)
+    const nameItem = getFieldDecorator('name', {
+      rules: [{ len: 24, required: true }],
+    })(<Input style={{ width: '300px' }} placeholder="请输入姓名" />);
     return (
-      <div 
-        style={{margin: '50px'}}
-      >
+      <div style={{ margin: '50px' }}>
         {/* <div style={{ position: 'relative', height: 100, width: 100, background: '#ddd' }}>
           <div style={{ position: 'sticky', color: 'red', top: 0}}>我是内容</div>
         </div> */}
-        <div 
+        {/* <div 
           style={{ width: 200, height: 200, border: '1px solid #ddd', float: 'right'}} 
           ref={(element) => this.node = element}
         >
           <div style={{ color: '#ff6900'}}>
             我是内容呀
           </div>
-        </div>
+        </div> */}
         <div>
-        <FormItem label="姓名" colon={false}>
-          {nameItem}
-          {/* {
+          <FormItem label="姓名" colon={false}>
+            {nameItem}
+            {/* {
             getFieldDecorator('name', { 
               initialValue: '', 
               rules:[{ min: 1, max: 24, message: '请输入正确的名字', required: true }]}
@@ -80,35 +114,42 @@ class AntdForm extends React.Component<Props>{
               <Input style={{width: '300px'}} placeholder="请输入姓名"/>
             )
           } */}
-        </FormItem>
-        <FormItem label="年龄" colon={false}>
-          {
-            getFieldDecorator('age', {
-              rules: [
-                { required: true, message: '请输入年龄'}, 
-                { validator: this.checkAge }
-              ]
+          </FormItem>
+          <FormItem label="年龄" colon={false}>
+            {getFieldDecorator('age', {
+              rules: [{ required: true, message: '请输入年龄' }, { validator: this.checkAge }],
             })(
-              <InputNumber 
-                style={{width: '200px'}} 
-                placeholder="请输入年龄" 
+              <InputNumber
+                style={{ width: '200px' }}
+                placeholder="请输入年龄"
                 min={1}
                 maxLength={3}
                 // formatter={(value: any) => `${value.toString().replace(/\D/g, '')}`}
               />
-            )
-          }
-        </FormItem>
-        <FormItem label="手机号" colon={false} required={true} help="长度1~11" hasFeedback={true} extra={true}>
-          <Input placeholder="请输入手机号"/>
-        </FormItem>
-        <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
-          Clear
-        </Button>
-        <Button onClick={this.submit}>提交</Button>
+            )}
+          </FormItem>
+          <FormItem label="选择日期" colon={false} required={true} help="指定日期">
+            <RangePicker
+              style={{ width: 'auto' }}
+              disabledDate={disabledDate}
+              disabledTime={disabledRangeTime}
+              showTime={{
+                hideDisabledOptions: true,
+                defaultValue: [moment('00:00:00', 'HH:mm:ss'), moment('11:59:59', 'HH:mm:ss')],
+              }}
+              format="YYYY-MM-DD HH:mm:ss"
+            />
+          </FormItem>
+          <FormItem label="手机号" colon={false} required={true} help="长度1~11" hasFeedback={true} extra={true}>
+            <Input placeholder="请输入手机号" />
+          </FormItem>
+          <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
+            Clear
+          </Button>
+          <Button onClick={this.submit}>提交</Button>
         </div>
       </div>
-    )
+    );
   }
   submit = (e: any) => {
     e.preventDefault();
@@ -117,7 +158,7 @@ class AntdForm extends React.Component<Props>{
         this.props.saveInfo(values);
       }
     });
-  }
+  };
   handleReset = () => {
     this.props.form.resetFields();
   };
--- my-app/src/pages/Loft/Index.tsx
+++ my-app/src/pages/Loft/Index.tsx
@@ -3,21 +3,61 @@ import './Index.less';
 import { Anchor }  from 'antd'
 import VirtualizedSelect from 'react-virtualized-select';
 // import 'react-virtualized-select/styles.css';
+import Swiper from 'swiper/dist/js/swiper';
+import 'swiper/dist/css/swiper.css';
 const { Link } = Anchor;
 
 export interface LoftState {
   githubUsers: any;
   selectedGithubUser: any;
+  bag: Array<any>;
+  classId: number;
+  arr: Array<any>;
 }
 export default class Loft extends React.Component<any, LoftState> {
   state: Readonly<LoftState> = {
     githubUsers: [],
-    selectedGithubUser: {}
+    selectedGithubUser: {},
+    bag: [
+      {
+        img: 'https://test.cdn.sunmi.com/IMG/154780678339675c41a83f60e07.jpg',
+        title: '第一屏'
+      },{
+        img: 'https://pic.cdn.sunmi.com/IMG/152777530938275b10004d5d718.jpg',
+        title: '第二屏'
+      },{
+        img: 'https://test.cdn.sunmi.com/IMG/156205496153045d1b113181847.jpg',
+        title: '第三屏'
+      }
+    ],
+    classId: 0,
+    arr: []
   }
   constructor(props: any){
     super(props);
   }
+  componentDidMount(){
+    var mySwiper = new Swiper('.swiper-container', {
+      on:{
+        lazyImageLoad: function(slideEl){
+          slideEl.setAttribute('data-loaded', 'true')
+        }
+      },
+      lazy: {
+        loadPrevNext: true
+      },
+      preloadImages: false,
+      slidesPerView: 1,
+      loop: true,
+      speed: 777,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      }
+    });
+  }
   render(){
+    let style = { backgroundImage: ''}
     return (
       <div className="container">
         <Anchor className="navbar">
@@ -45,6 +85,27 @@ export default class Loft extends React.Component<any, LoftState> {
             value={this.state.selectedGithubUser}
             valueKey='id'
           />
+        </div>
+        <div className='swiper-box'>
+          <div className="swiper-button-prev swiper-button-white swiper-button"></div>
+          <div className='swiper-container'>
+            <div className='swiper-wrapper'>
+              {
+                  this.state.bag.map((item,index)=>{   // this.state.bag是在state里面定义的数组为了循环数据
+                      style.backgroundImage = `url(${item.img})`
+                      return(                                    
+                        <div 
+                          key={index} 
+                          className="swiper-slide swiper-lazy"
+                        >
+                          <img src={item.img} alt=""/>
+                        </div>
+                      )
+                  })
+              }
+            </div>
+          </div>
+          <div className="swiper-button-next swiper-button-white swiper-button"></div>
         </div>
       </div>
     )
